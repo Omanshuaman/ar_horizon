e5aa2d0023b69621647e546b5785c1143cae9dc8
Omanshuamanarnxt <omanshuaman@email.com>
2023-12-07 17:29:00 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ARImage from "../pages/ARImage";
+import ARImage from "./pages/ARImage";
 function App() {
   return (
     <BrowserRouter>
