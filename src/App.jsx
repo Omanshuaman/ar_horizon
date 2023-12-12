bd581739beb703b6efba2bf37b0ab8eeacaed758
Omanshuamanarnxt <omanshuaman@email.com>
2023-12-12 13:10:57 +0530

    dcd

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,19 @@ import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ARImage from "./ARImage";
 import Html from "./Html";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    // Your code to run when the component mounts
+    window.location.href = "../src/studio-master/pages/marker/index.html";
+
+    // Cleanup function (optional) - will be called when the component unmounts
+    return () => {
+      // Optional cleanup code goes here
+    };
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
