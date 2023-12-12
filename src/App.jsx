0f787e6964debe19f7b91541ef8d79d2a2ce0732
Omanshuamanarnxt <omanshuaman@email.com>
2023-12-12 13:15:08 +0530

    v

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,45 @@ function App() {
   const [error, setError] = useState(null);
   console.log("first");
   const param = useParams();
-  useEffect(() => {
-    // Your code to run when the component mounts
-    window.location.href = "../src/studio-master/pages/marker/index.html";
 
-    // Cleanup function (optional) - will be called when the component unmounts
-    return () => {
-      // Optional cleanup code goes here
-    };
-  }, []);
-  return <>dvd</>;
+  return (
+    <>
+      <a-scene
+        vr-mode-ui="enabled: false;"
+        loading-screen="enabled: false;"
+        renderer="logarithmicDepthBuffer: true;"
+        arjs="trackingMethod: best; sourceType: webcam; debugUIEnabled: false;"
+        id="scene"
+        embedded
+        gesture-detector>
+        <a-assets>
+          <a-asset-item
+            id="animated-asset"
+            src="https://jobpostingbucket.s3.ap-south-1.amazonaws.com/model/Pizza+Flip+Animation+(1).glb"></a-asset-item>
+        </a-assets>
+
+        <a-marker
+          id="animated-marker"
+          type="pattern"
+          preset="custom"
+          url="https://jobpostingbucket.s3.ap-south-1.amazonaws.com/markerPattern1702035702894.patt"
+          raycaster="objects: .clickable"
+          emitevents="true"
+          cursor="fuse: false; rayOrigin: mouse;"
+          id="markerA">
+          <a-entity
+            id="bowser-model"
+            scale="2.078150572818595 2.078150572818595 2.078150572818595"
+            animation-mixer="loop: repeat"
+            gltf-model="#animated-asset"
+            class="clickable"
+            gesture-handler></a-entity>
+        </a-marker>
+
+        <a-entity camera></a-entity>
+      </a-scene>
+    </>
+  );
 }
 
 export default App;
